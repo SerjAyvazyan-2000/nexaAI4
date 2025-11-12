15758b83aae89c1d3fd4e5f4c6d3059947e8c12a
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-11-12 23:56:37 +0400

    Serzh Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -135,3 +135,102 @@ document.querySelectorAll('.menu-list-link').forEach(link => {
 
 
 
+const animItem = document.querySelector(".anmiation-item--one");
+const imageWrap = document.querySelector(".anmiation-small-img");
+const imgLow = document.querySelector(".anmiation-small-img .low-quality");
+const imgHigh = document.querySelector(".anmiation-small-img .high-quality");
+const mouse = document.querySelector(".anmiation-mouse");
+
+let direction = 1;
+let x = 0;
+let isFirstRun = true; 
+
+function mouseClick() {
+  mouse.classList.remove("reset");
+  mouse.classList.add("click");
+
+  setTimeout(() => {
+    animate(); 
+  }, 450);
+}
+
+function animate() {
+  const itemRect = animItem.getBoundingClientRect();
+  const imgRect = imageWrap.getBoundingClientRect();
+  const screenWidth = window.innerWidth; 
+  const limitOffset = screenWidth <= 576 ? 30 : 170; 
+  const centerLine = itemRect.left + itemRect.width / 2;
+  const imageCenter = imgRect.left + imgRect.width / 2;
+
+  if (imageCenter > centerLine) {
+    imgLow.style.display = "none";
+    imgHigh.style.display = "block";
+    animItem.classList.add("active");
+  } else {
+    imgLow.style.display = "block";
+    imgHigh.style.display = "none";
+    animItem.classList.remove("active");
+  }
+
+  x += 2 * direction;
+  imageWrap.style.transform = `translateX(${x}px)`;
+
+  
+   if (x >= itemRect.width - imgRect.width - limitOffset) {
+    direction = -1;
+  }
+
+  if (x <= 0) {
+    direction = 1;
+    imageWrap.style.transform = `translateX(${x}px)`;
+
+    mouse.classList.remove("click");
+    mouse.classList.add("reset");
+
+    setTimeout(mouseClick, 700);
+    return; 
+  }
+
+  requestAnimationFrame(animate);
+}
+
+mouseClick();
+
+
+
+const anmiationBlock = document.querySelector(".anmiation-item--two");
+
+function animateCompare() {
+  anmiationBlock.classList.add("active");
+
+  setTimeout(() => {
+    anmiationBlock.classList.remove("active");
+  }, 2500);
+}
+
+animateCompare();
+setInterval(animateCompare, 5000);
+
+
+
+
+
+
+
+
+const blockTree = document.querySelector(".anmiation-item--tree");
+
+function animateMouseZoom() {
+  blockTree.classList.add("active");
+
+  // плавное возвращение
+  setTimeout(() => {
+    blockTree.classList.remove("active");
+  }, 3000);
+}
+
+// первый запуск
+animateMouseZoom();
+
+// повтор каждые 5 секунд
+setInterval(animateMouseZoom, 5000);
